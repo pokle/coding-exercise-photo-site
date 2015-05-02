8795e6ed1dd1ad292aae56ca1c7d21ae2944d0b7
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-03 00:50:25 +1000

    refactor: re-org'd

--- generate.rb
+++ generate.rb
@@ -5,14 +5,31 @@ module Generate
   Image = Struct.new :thumb, :full, :make, :model
   Navigation = Struct.new :title, :path
 
+  #
+  # Images
+  #
   def Generate.distinct_makes(images) 
     images.collect(&:make).uniq
   end
 
+  def Generate.images_with_make(images, make)
+    images.find_all {|image| make == image.make }
+  end
+
+  #
+  # Navigations
+  #
   def Generate.navigate_to_make(make)
     Navigation.new(make, "#{make}.html")
   end
 
+  def Generate.navigate_to_model(image)
+    Navigation.new(image.model, "#{image.make}-#{image.model}.html")
+  end
+
+  #
+  # Pages
+  #
   def Generate.index(images)
     {
       path: "index.html",
@@ -22,18 +39,13 @@ module Generate
     }
   end
 
-  def Generate.navigate_to_model(image)
-    Navigation.new(image.model, "#{image.make}-#{image.model}.html")
-  end
-
   def Generate.makes(images)
     distinct_makes(images)
       .collect do |make| 
-        images_of_this_make = images.find_all {|image| make == image.make }
         {
           path:   "#{make}.html",
-          images: images_of_this_make.take(10),
-          navigations: images_of_this_make
+          images: images_with_make(images, make).take(10),
+          navigations: images_with_make(images, make)
                         .uniq(&:model)
                         .collect {|image| navigate_to_model(image)}
         }
