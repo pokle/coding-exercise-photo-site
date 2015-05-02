a51972b0441100f99291f142b3a484405f488183
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-02 23:12:53 +1000

    Partially generated the makes representation.

--- generate.rb
+++ generate.rb
@@ -2,13 +2,31 @@
 
 module Generate
 
-  Image = Struct.new :thumb, :full, :make
+  Image = Struct.new :thumb, :full, :make, :model
+  Navigation = Struct.new :title, :path
+
+  def Generate.distinct_makes(works) 
+    works[:images].collect(&:make).uniq
+  end
 
   def Generate.index(works)
     {
-      images: works[:images].take(10).collect(&:thumb),
-      makes:  works[:images].collect(&:make).uniq
+      path: "index.html",
+      images: works[:images].take(10),
+      navigations:  distinct_makes(works)
+                    .collect{|make| Navigation.new(make, "#{make}.html")}
     }
+  end
+
+  def Generate.makes(works)
+    distinct_makes(works)
+      .collect do |make| 
+        {
+          path:   "#{make}.html",
+          images: works[:images].find_all {|image| make == image.make },
+          navigations: []
+        }
+      end
   end
 
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -16,18 +16,58 @@ RSpec.describe Generate, ".index" do
     expect(Generate.index(dummy_works(11))[:images].count).to eq 10
   end
 
-  it "contains a list of distinct make names" do
+
+  it "transforms works into an intermediate view representation for index pages" do
 
     works = {
       images: [
-        Generate::Image.new("", "", "sony"),
-        Generate::Image.new("", "", "sony"),
-        Generate::Image.new("", "", "leica")
+        sony1 = Generate::Image.new(nil, nil, "sony"),
+        sony2 = Generate::Image.new(nil, nil, "sony"),
+        leica1 = Generate::Image.new(nil, nil, "leica"),
       ]
     }
 
-    expect(Generate.index(works)[:makes]).to eq ["sony", "leica"]
+    expect(Generate.index(works)).to eq({
+      path: "index.html",
+      images: [sony1, sony2, leica1],
+      navigations: [
+        Generate::Navigation.new("sony", "sony.html"),
+        Generate::Navigation.new("leica", "leica.html")
+      ]
+    })
 
   end
 
+end
+
+RSpec.describe Generate, ".makes" do
+  
+  it "transforms works into an intermediate representation for make pages" do
+    
+    works = {
+      images: [
+        sony1 = Generate::Image.new(nil, nil, "sony"),
+        sony2 = Generate::Image.new(nil, nil, "sony"),
+        leica1 = Generate::Image.new(nil, nil, "leica"),
+      ]
+    }
+
+    expect(Generate.makes(works)).to eq(
+      [
+        {
+          path: "sony.html",
+          images: [sony1, sony2],
+          navigations: []#[Generate::Navigation.new("dx1", "sony-dx1.html")]
+        },
+        
+        {
+          path: "leica.html",
+          images: [leica1],
+          navigations: []#[Generate::Navigation.new("m1", "leica-m1.html")]
+        }
+
+      ]
+    )
+
+  end
 end
