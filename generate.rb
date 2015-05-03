745806a3e2a3ab46a9ccd03c3f7749cd5e9884b1
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-03 11:15:24 +1000

    plugged everything together for the first time

--- generate.rb
+++ generate.rb
@@ -1,8 +1,22 @@
 #!/usr/bin/env bundle exec ruby
 
-$: << 'lib'
-
-require 'page'
+$: << __dir__ + '/lib'
 require 'parse'
+require 'page'
+require 'render'
 
-puts "Nothing here yet"
+
+if ARGV.length != 2
+    STDERR.puts "usage: WORKS-FILE OUTPUT-DIR"
+    exit 1
+end
+
+(works_file, output_dir) = ARGV
+FileUtils.mkdir_p output_dir
+
+images = Parse::works(IO::read(works_file))
+page = Page::index(images)
+rendered = Render::page(Render::TEMPLATE, page)
+out_file = File.join(output_dir, page[:path])
+IO::write(out_file, rendered) 
+puts "Wrote to #{out_file}"
