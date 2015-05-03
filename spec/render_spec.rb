d6c1c596c564c27850b19fc3f07b0f899699d9ea
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-03 10:44:24 +1000

    rendered navigations

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -1,4 +1,5 @@
 require 'parse'
+require 'page'
 require 'render'
 
 RSpec.describe Render do
@@ -26,6 +27,23 @@ RSpec.describe Render do
 
         expect(rendering).to match %q{<a href="full1"><img src="thumb1"></a>}
         expect(rendering).to match %q{<a href="full2"><img src="thumb2"></a>}
+       
+    end
+
+    it "renders the page navigation" do
+        page = {
+            navigations: 
+            [
+                Page::Navigation.new('nav1', 'http://nav1'),
+                Page::Navigation.new('nav2', 'http://nav2')
+            ]
+        }
+
+        rendering = Render::page(Render::TEMPLATE, page)
+
+        expect(rendering).to match %q{<a href="http://nav1">nav1</a>}
+        expect(rendering).to match %q{<a href="http://nav2">nav2</a>}
+        
        
     end
 
