7b1dbbfafb5db46e6112903c1ea0b66c8cd13ed6
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-03 11:23:29 +1000

    refactor: extracted common stuff

--- generate.rb
+++ generate.rb
@@ -6,6 +6,14 @@ require 'page'
 require 'render'
 
 
+def render_and_write_out(output_dir, page)
+    rendered = Render::page(Render::TEMPLATE, page)
+    out_file = File.join(output_dir, page[:path])
+    IO::write(out_file, rendered) 
+    puts "Generated #{out_file}"
+end
+
+
 if ARGV.length != 2
     STDERR.puts "usage: WORKS-FILE OUTPUT-DIR"
     exit 1
@@ -14,9 +22,8 @@ end
 (works_file, output_dir) = ARGV
 FileUtils.mkdir_p output_dir
 
+
+
+    
 images = Parse::works(IO::read(works_file))
-page = Page::index(images)
-rendered = Render::page(Render::TEMPLATE, page)
-out_file = File.join(output_dir, page[:path])
-IO::write(out_file, rendered) 
-puts "Wrote to #{out_file}"
+render_and_write_out(output_dir, Page::index(images))
